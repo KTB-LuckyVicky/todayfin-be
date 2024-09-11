3edfb4ce3044fb6aa733d83679e5aab8421fdfa6
미나 이 <chsjen492@gmail.com>
2024-09-11 22:37:22 +0900

    refactor: user middleware error handling

--- src/middlewares/user.ts
+++ src/middlewares/user.ts
@@ -15,8 +15,9 @@ export async function verifyUser(req: Request, res: Response, next: NextFunction
         const payload = verify(jwt, SECRET_KEY) as userJWTPayload
         const row = await (await conn).query('select * from User where oauthId=?', payload.oauthId)
         req.user = row[0]
+
         next()
     } catch (error) {
-        throw new Error('middleware error')
+        res.sendStatus(401)
     }
 }
